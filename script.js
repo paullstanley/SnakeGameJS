ad9b87ae3131f656e463197a5cdbd3fac5e8ace5
Paull Stanley <paull.stanley@me.com>
2022-10-22 16:38:47 -0400

    Update script.js
    refactoring

--- script.js
+++ script.js
@@ -98,17 +98,17 @@ function drawGame(){
     if (event.touches && event.touches[0]) {
         x = event.touches[0].clientX;
         y = event.touches[0].clientY;
-        yvelocity, xvelocity = y, x;
+        yvelocity, xvelocity = y - y + 1, x - x + 1;
         changeSnakePosition();
     } else if (event.originalEvent && event.originalEvent.changedTouches[0]) {
         x = event.originalEvent.changedTouches[0].clientX;
         y = event.originalEvent.changedTouches[0].clientY;
-        yvelocity, xvelocity = y, x;
+        yvelocity, xvelocity = y - y + 1, x - x + 1;
         changeSnakePosition();
     } else if (event.clientX && event.clientY) {
         x = event.clientX;
         y = event.clientY;
-        yvelocity, xvelocity = y, x;
+        yvelocity, xvelocity = y - y + 1, x - x + 1;
         changeSnakePosition();
     }
 
